47c6f7dc5b0e450b0289d4bf9c9a56ba1b3a2881
BaraaBaghdadi <mhdBaraaAlBaghdadi@gmail.com>
2025-07-22 13:20:31 +0300

    add confirm-dialog

--- ui/src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
+++ ui/src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './confirmation-dialog.scss'
 })
 export class ConfirmationDialog {
+ @Input() title!: string;
+  @Input() message!: string;
+  @Input() btnOkText!: string;
+  @Input() btnCancelText!: string;
+
+  constructor(private activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+  public decline() {
+    this.activeModal.close(false);
+  }
+
+  public accept() {
+    this.activeModal.close(true);
+  }
+
+  public dismiss() {
+    this.activeModal.dismiss();
+  }
 
 }
